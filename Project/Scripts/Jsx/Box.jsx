a583cf4e5a7fd7835e30a1653c143819f8b1110b
Yayuan <yyhu@thoughtworks.com>
2016-09-29 18:10:18 +0800

    [Layout Adjustment] import real box data, and other style change.

--- Project/Scripts/Jsx/Box.jsx
+++ Project/Scripts/Jsx/Box.jsx
@@ -13,10 +13,10 @@
         <div className="w3-container w3-card-2 w3-white w3-round w3-margin">
           <br/>
             <img src="Content/Images/Logo.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width: 60}} />
-              <h4>John Doe</h4><br/>
+              <h4>前方高能</h4><br/>
               <hr className="w3-clear" />
                 <p>{this.props.data.Title}</p>
-                <img src={this.props.data.Url} style={{flex: 1}} className="w3-margin-bottom"/>
+				  <img src={this.props.data.Url} className="w3-margin-bottom"/>
 
                   <div className="w3-panel w3-leftbar">
                     <p>
