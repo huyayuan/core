6e197ae228dd5ca1559577cb142ab3701782167f
Yayuan <huyayuanme@163.com>
2016-10-02 22:58:06 +0800

    [Frontend Improvement] Adjust logo, and footer

--- Project/Scripts/Jsx/Box.jsx
+++ Project/Scripts/Jsx/Box.jsx
@@ -14,14 +14,14 @@
 
     if(this.props.data.Type === 1)
     {
-        img = <img src={this.props.data.Url} className="w3-margin-bottom"/>;
+        img = <img src={this.props.data.Url} style={{}} className="w3-margin-bottom width-100"/>;
     }
 
     return (
         <div className="w3-container w3-card-2 w3-white w3-round w3-margin">
           <br/>
             <img src="Content/Images/Logo.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width: 60}} />
-              <h4>前方高能</h4><br/>
+              <h4>高能楼主</h4><br/>
               <hr className="w3-clear" />
                 <p>{this.props.data.Title}</p>
                     {img}
