689a3ec22da68a9e0a1e9e51275d882b84eb037c
Yayuan <huyayuanme@163.com>
2016-10-24 13:23:50 +0800

    [UI] change baidu fenxiang style

--- Project/Scripts/Js/baidufenxiang.js
+++ Project/Scripts/Js/baidufenxiang.js
@@ -1,18 +1,1 @@
-﻿window._bd_share_config = {
-    "common": {
-        "bdSnsKey": {},
-        "bdText": "前方高能！有猛料~",
-        "bdMini": "1",
-        "bdMiniList": ["tsina", "weixin", "sqq", "tqq", "tqf", "youdao", "qzone", "copy"],
-        "bdPic": "",
-        "bdStyle": "0",
-        "bdSize": "16"
-    },
-    "slide": { "type": "slide", "bdImg": "0", "bdPos": "right", "bdTop": "250" },
-    "image": { "viewList": ["qzone", "tsina", "tqq", "renren", "weixin"], "viewText": "我要分享到：", "viewSize": "16" }
-};
-with (document)
-    0[(getElementsByTagName('head')[0] || body)
-        .appendChild(createElement('script'))
-        .src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' +
-        ~(-new Date() / 36e5)];
+﻿window._bd_share_config = { "common": { "bdSnsKey": {}, "bdText": "没时间解释了，快上车！老司机带你飞～", "bdMini": "1", "bdMiniList": false, "bdPic": "", "bdStyle": "1", "bdSize": "16" }, "slide": { "type": "slide", "bdImg": "7", "bdPos": "right", "bdTop": "250" }, "image": { "viewList": ["weixin", "qzone", "sqq", "tsina", "tqq"], "viewText": "分享到：", "viewSize": "24" } }; with (document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];
